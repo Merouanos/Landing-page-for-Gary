93c696faa1f646cad88f862f7da01350a7e3f910
Merouanos <jessemerouane@gmail.com>
2026-03-15 02:32:37 +0100

    finished the hero section

---
+++ script.js
@@ -0,0 +1,23 @@
+const mainImages = [
+    '/images/image4.jpg',
+    '/images/image1.jpg', 
+    '/images/image3.jpg',
+    
+];
+
+let currentIndex = 0;
+const heroImg = document.getElementById('hero-main-img');
+
+function alternateImages() {
+    heroImg.style.opacity = '0';
+
+    setTimeout(() => {
+        currentIndex = (currentIndex + 1) % mainImages.length;
+        heroImg.src = mainImages[currentIndex];
+
+        heroImg.style.opacity = '1';
+    }, 1000); 
+}
+
+
+setInterval(alternateImages, 5000);
